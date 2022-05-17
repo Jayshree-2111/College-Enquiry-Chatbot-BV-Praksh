3166bf1f0e97d55a399bd6d4b262ab955666c88e
Jayshree Gupta <86256037+Jayshree-2111@users.noreply.github.com>
2022-05-17 15:25:06 +0530

    Update credentials.php

--- Chatbot and Website Source Code/Source Code/credentials.php
+++ Chatbot and Website Source Code/Source Code/credentials.php
@@ -6,6 +6,6 @@ $password = "";
 $database = "onlinebot";
 
 $conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
-    define("EMAILID",'bvChatbot.praksh@gmail.com');
-    define('PASSWORD','JayKhu2@22');
+    define("EMAILID",'Enter Your Email Id, from which you want to send mail'); //Enter your E-mail
+    define('PASSWORD','Enter the password of the above entered e-mail'); //Enter your E-mail Account Password
 ?>
